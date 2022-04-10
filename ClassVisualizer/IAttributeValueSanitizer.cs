c31bd36f95bd8f26b276797b803a4cee417c8725
artakhak <artakhak@yahoo.com>
2022-04-10 15:54:04 -0400

    Small text fix in license texts

--- ClassVisualizer/IAttributeValueSanitizer.cs
+++ ClassVisualizer/IAttributeValueSanitizer.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) IoC.Configuration Project. All rights reserved.
+﻿// Copyright (c) ClassVisualizer Project. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the solution root for license information.
 
 using JetBrains.Annotations;
